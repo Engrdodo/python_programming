e577dca070d9f8a881d90eec9c711ac441fd5a3e
Akinola David Olawale <akinoladavidolawale@gmail.com>
2023-12-31 11:56:42 +0100

    Keeps tracks of the players' scores

--- game/dice.py
+++ game/dice.py
@@ -21,8 +21,34 @@ while True:
 
 
 max_score = 50
-player_scores = [3 for i in range(players)]
-print(player_scores)
+player_scores = [0 for i in range(players)]
+
+while max(player_scores) < max_score:
+    for player_index in range(players):
+        print("\nPlayer number",player_index +1, "turn has started!")
+        print("Your total score is:", player_scores[player_index], "\n")
+
+        current_score = 0
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You roll a:", current_score)
+        player_scores[player_index] += current_score
+        print("Your total score is:", player_scores[player_index])
+
+max_score =max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player number", winning_index + 1, "is the winner with a score of:", max_score)
+
+        
 
 
 
