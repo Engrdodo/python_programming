5dad034230b5f1a58cdc92a1c95f6f6f614a9abe
Akinola David Olawale <akinoladavidolawale@gmail.com>
2024-01-01 13:42:42 +0100

    Pushed day_1 of #100DaysOfCode challenge

---
+++ 100_days_python/README.md
@@ -0,0 +1,2 @@
+These projects are coming from Dr Angela Yu 100DaysOfCode challenge
+Day 1----- Band Name generator program
---
+++ 100_days_python/band_name_generator01.py
@@ -0,0 +1,6 @@
+#This program generates band name based on inputs of the user
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your Band name could be {city} {pet_name}")
