5b0f648b82e58c0959b889ba1ce1c50cb3aea112
Akinola David Olawale <akinoladavidolawale@gmail.com>
2024-01-11 22:22:57 +0100

    fix the hangman game bugs

--- 100_days_python/hangman.py
+++ 100_days_python/hangman.py
@@ -1,10 +1,13 @@
 import random
 from hangman_words import word_list
 from hangman_art import logo, stages
+# from replit import clear
 
 print(logo)
+print("Welcome to DAVETECH HANGMAN GAME, You've got six chances to save your man")
 chosen_word = random.choice(word_list)
-print(chosen_word)
+word_length = len(chosen_word)
+print(f"The word is '{chosen_word}':(testing purpose)")
 display = []
 end_of_game = False
 lives = 6
@@ -13,25 +16,23 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter:\n").lower()
     
-    chosen_word_list = list(chosen_word)
-    print(chosen_word_list)
-    for char in chosen_word:
-        if char == guess:
-            display.append(guess)
-        else:
-            display.append('_')
-    print(display)
+    # clear()-----clear function from replit to clear hangman art stages backlog
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+  
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(f"{' '.join(display)}")
     if "_" not in display:
         end_of_game = True
-        print("You win")
-#     else:
-#         print("You win")
-# # for 
-
-# guess = input("Guess a letter:\n").lower()
-
-# for letter in chosen_word:
-#     if letter == guess:
-#         print("right")
-#     else:
-#         print("wrong")
+        print("You win.")
+    print(stages[lives])
